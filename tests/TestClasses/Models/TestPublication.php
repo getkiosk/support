69e684bb6aa6f0f6675b70ba5dc67b6218505cce
Nasyrov Evgenii <inasyrov@ya.ru>
2021-06-01 15:31:52 +0100

    Add test publication collection

--- tests/TestClasses/Models/TestPublication.php
+++ tests/TestClasses/Models/TestPublication.php
@@ -5,6 +5,7 @@ namespace Kiosk\Support\Tests\TestClasses\Models;
 use Illuminate\Database\Eloquent\Model;
 use Kiosk\Support\Concerns\HasPublicationHelpers;
 use Kiosk\Support\Concerns\UnguardsAttributes;
+use Kiosk\Support\Tests\TestClasses\Collections\TestPublicationCollection;
 use Kiosk\Support\Tests\TestClasses\QueryBuilders\TestPublicationQueryBuilder;
 
 class TestPublication extends Model
@@ -17,5 +18,10 @@ class TestPublication extends Model
     public function newEloquentBuilder($query): TestPublicationQueryBuilder
     {
         return new TestPublicationQueryBuilder($query);
+    }
+
+    public function newCollection(array $models = []): TestPublicationCollection
+    {
+        return new TestPublicationCollection($models);
     }
 }
