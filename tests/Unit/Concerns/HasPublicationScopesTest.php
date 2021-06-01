d082a00bf4c87b512ef30cca5b5335f77756238d
Nasyrov Evgenii <inasyrov@ya.ru>
2021-06-01 15:29:15 +0100

    Test publication-related scopes

--- tests/Unit/Concerns/HasPublicationScopesTest.php
+++ tests/Unit/Concerns/HasPublicationScopesTest.php
@@ -3,12 +3,52 @@
 namespace Kiosk\Support\Tests\Unit\Models\Concerns;
 
 use Kiosk\Support\Tests\TestCase;
+use Kiosk\Support\Tests\TestClasses\Models\TestPublication;
 
 class HasPublicationScopesTest extends TestCase
 {
     /** @test */
-    public function it_provides_draft_scope(): void
+    public function it_scopes_query_to_only_include_draft_models(): void
     {
-        //
+        $model1 = TestPublication::create(['published_at' => null]);
+        $model2 = TestPublication::create(['published_at' => now()]);
+        $model3 = TestPublication::create(['published_at' => now()->addDay()]);
+
+        $models = TestPublication::query()
+            ->draft()
+            ->get();
+
+        $this->assertCount(1, $models);
+        $this->assertTrue($models->contains($model1));
+    }
+
+    /** @test */
+    public function it_scopes_query_to_only_include_published_models(): void
+    {
+        $model1 = TestPublication::create(['published_at' => null]);
+        $model2 = TestPublication::create(['published_at' => now()]);
+        $model3 = TestPublication::create(['published_at' => now()->addDay()]);
+
+        $models = TestPublication::query()
+            ->published()
+            ->get();
+
+        $this->assertCount(1, $models);
+        $this->assertTrue($models->contains($model2));
+    }
+
+    /** @test */
+    public function it_scopes_query_to_only_include_scheduled_models(): void
+    {
+        $model1 = TestPublication::create(['published_at' => null]);
+        $model2 = TestPublication::create(['published_at' => now()]);
+        $model3 = TestPublication::create(['published_at' => now()->addDay()]);
+
+        $models = TestPublication::query()
+            ->scheduled()
+            ->get();
+
+        $this->assertCount(1, $models);
+        $this->assertTrue($models->contains($model3));
     }
 }
