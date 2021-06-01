022e9d0e06ebf26492bcf5316adcd6fdd731ef4e
Nasyrov Evgenii <inasyrov@ya.ru>
2021-06-01 15:23:53 +0100

    Apply unguards attribute concern

--- tests/TestClasses/Models/TestPublication.php
+++ tests/TestClasses/Models/TestPublication.php
@@ -4,10 +4,12 @@ namespace Kiosk\Support\Tests\TestClasses\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Kiosk\Support\Concerns\HasPublicationHelpers;
+use Kiosk\Support\Concerns\UnguardsAttributes;
 
 class TestPublication extends Model
 {
     use HasPublicationHelpers;
+    use UnguardsAttributes;
 
     public $timestamps = false;
 }
